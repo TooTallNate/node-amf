2d5af891cacf598b6c889e7038f9b53ff87eb120
Nathan Rajlich <nathan@tootallnate.net>
2013-02-24 12:42:31 -0800

    test: refactoring of a few tests

--- test/read.js
+++ test/read.js
@@ -53,37 +53,6 @@ describe('read()', function () {
     assert.strictEqual(undefined, obj);
   });
 
-  it('should read a Person object', function () {
-    var data = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'person.amf0'));
-    var obj = amf.read(data, 0);
-    assert.deepEqual(obj, { name: 'Mike', age: '30', alias: 'Mike' });
-  });
-
-  it('should read an FLV metadata object', function () {
-    var data = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'metadata.amf'));
-    var offset = 0;
-    var info = { byteLength: 0 };
-
-    var obj = amf.read(data, offset, info);
-    assert.equal('onMetaData', obj);
-    assert.equal(13, info.byteLength);
-
-    offset += info.byteLength;
-    info.byteLength = 0;
-
-    obj = amf.read(data, offset, info);
-    assert(Array.isArray(obj));
-    assert.equal(0, obj.length);
-    assert.equal(7.211, obj.duration);
-    assert.equal(187.5, obj.audiodatarate);
-    assert.equal(22050, obj.audiosamplerate);
-    assert.equal(16, obj.audiosamplesize);
-    assert.equal(false, obj.stereo);
-    assert.equal(2, obj.audiocodecid);
-    assert.equal('Lavf54.29.104', obj.encoder);
-    assert.equal(138064, obj.filesize);
-  });
-
   it('should read a "strict array"', function () {
     var data = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'amf0-strict-array.bin'));
 
@@ -102,6 +71,78 @@ describe('read()', function () {
     var date = amf.read(data, 0);
     assert(util.isDate(date));
     assert.equal(1590796800000, date.getTime());
+  });
+
+  it('should read an "untyped object"', function () {
+    var data = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'amf0-untyped-object.bin'));
+
+    var obj = amf.read(data, 0);
+    assert.deepEqual({ baz: null, foo: 'bar' }, obj);
+  });
+
+  describe('FLV metadata', function () {
+
+    it('should read a basic "name" and "value" from an FLV metadata packet', function () {
+      var data = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'amf0-flv-metadata.bin'));
+      var info = { offset: 0 };
+
+      var name = amf.read(data, info);
+      assert.equal('onMetaData', name);
+      assert.equal(13, info.byteLength);
+
+      var obj = amf.read(data, info);
+      assert(Array.isArray(obj));
+      assert.equal(0, obj.length);
+      assert.equal(7.211, obj.duration);
+      assert.equal(187.5, obj.audiodatarate);
+      assert.equal(22050, obj.audiosamplerate);
+      assert.equal(16, obj.audiosamplesize);
+      assert.equal(false, obj.stereo);
+      assert.equal(2, obj.audiocodecid);
+      assert.equal('Lavf54.29.104', obj.encoder);
+      assert.equal(138064, obj.filesize);
+
+      assert.equal(info.offset, data.length);
+    });
+
+    it('should read a complex "name" and "value" from an FLV metadata packet', function () {
+      var data = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'amf0-flv-metadata-2.bin'));
+      var info = { offset: 0 };
+
+      var name = amf.read(data, info);
+      assert.equal('onMetaData', name);
+      assert.equal(13, info.byteLength);
+
+      var obj = amf.read(data, info);
+      assert(Array.isArray(obj));
+      assert.equal(obj.duration, 174.986);
+      assert.equal(obj.moovPosition, 32);
+      assert.equal(obj.width, 640);
+      assert.equal(obj.height, 360);
+      assert.equal(obj.videocodecid, 'avc1');
+      assert.equal(obj.audiocodecid, 'mp4a');
+      assert.equal(obj.avcprofile, 77);
+      assert.equal(obj.avclevel, 30);
+      assert.equal(obj.aacaot, 2);
+      assert.equal(obj.videoframerate, 25);
+      assert.equal(obj.audiosamplerate, 44100);
+      assert.equal(obj.audiochannels, 2);
+      assert.deepEqual(obj.trackinfo, [
+        { length: 4374000,
+          timescale: 25000,
+          language: 'eng',
+          sampledescription: [ { sampletype: 'avc1' } ]
+        },
+        { length: 7716864,
+          timescale: 44100,
+          language: 'eng',
+          sampledescription: [ { sampletype: 'mp4a' } ]
+        }
+      ]);
+
+      assert.equal(info.offset, data.length);
+    });
+
   });
 
 });
