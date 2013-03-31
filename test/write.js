ff21f363fded1c9fc8202eeebf57566e7402481b
Nathan Rajlich <nathan@tootallnate.net>
2013-03-31 14:57:09 -0700

    write: more progress on the AMF0 write() function
    Adds Boolean, String, and ECMA Object type support.

--- test/write.js
+++ test/write.js
@@ -18,4 +18,32 @@ describe('write()', function () {
     assert.deepEqual(data, buf);
   });
 
+  it('should write a Boolean `true` value', function () {
+    var data = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'amf0-boolean-true.bin'));
+    var buf = new Buffer(data.length);
+    amf.write(buf, true, 0);
+    assert.deepEqual(data, buf);
+  });
+
+  it('should write a Boolean `false` value', function () {
+    var data = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'amf0-boolean-false.bin'));
+    var buf = new Buffer(data.length);
+    amf.write(buf, false, 0);
+    assert.deepEqual(data, buf);
+  });
+
+  it('should write a String value', function () {
+    var data = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'amf0-string.bin'));
+    var buf = new Buffer(data.length);
+    amf.write(buf, 'this is a テスト', 0);
+    assert.deepEqual(data, buf);
+  });
+
+  it('should write an Object value', function () {
+    var data = fs.readFileSync(path.resolve(__dirname, 'fixtures', 'amf0-object.bin'));
+    var buf = new Buffer(data.length);
+    amf.write(buf, { bar: 3.14, foo: 'baz' }, 0);
+    assert.deepEqual(data, buf);
+  });
+
 });
